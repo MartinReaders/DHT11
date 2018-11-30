17e3a18633ae3f262f235d4a52861758ceae3f66
MartinReaders <martin.khapherian@gmail.com>
2018-11-30 10:22:13 +0100

    ajout css

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<h1>Température</h1>
+	
+	<?php 
 
-echo ("hello sff world");
+echo ("hello world");
 
 
  ?>
+
+</body>
+</html>
+
+
